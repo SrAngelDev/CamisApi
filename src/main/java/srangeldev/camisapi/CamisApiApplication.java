1230188b674c7f700ce3390acbdf4872d8ec6faa
Ángel Sánchez Gasanz <129800127+SrAngelDev@users.noreply.github.com>
2025-11-05 16:58:15 +0100

    initial commit

---
+++ src/main/java/srangeldev/camisapi/CamisApiApplication.java
@@ -0,0 +1,13 @@
+package srangeldev.camisapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamisApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CamisApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/srangeldev/camisapi/CamisApiApplicationTests.java
@@ -0,0 +1,13 @@
+package srangeldev.camisapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamisApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
