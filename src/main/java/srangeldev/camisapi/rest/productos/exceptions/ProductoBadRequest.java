0dd1e23888b47643108c83d0b70cfc14c938afad
JorgeMrj <roadtoello2@gmail.com>
2025-11-10 18:22:11 +0100

    Productos Exceptions add

---
+++ src/main/java/srangeldev/camisapi/rest/productos/exceptions/ProductoBadRequest.java
@@ -0,0 +1,8 @@
+package srangeldev.camisapi.rest.productos.exceptions;
+
+
+public class ProductoBadRequest extends ProductoException {
+    public ProductoBadRequest(String message) {
+        super(message);
+    }
+}
